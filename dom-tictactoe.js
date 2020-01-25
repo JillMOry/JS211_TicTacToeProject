f83f80c79efcbcd9f897801bd4d44f5594079372
JillMOry <oryjill@gmail.com>
2020-01-25 09:58:01 -0600

    Cleaned up code and added comments

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -1,13 +1,3 @@
-//       ***********************
-//            INSTRUCTIONS
-//       ***********************
-
-// 1. Read the code below and figure out the data flow
-// 2. Add in your code from the terminal app (check for win logic)
-// 3. Look for the @TODO, to fix
-// next to each @TODO you will find tasks that need to be finished
-// 4. GET THIS GAME WORKING!!
-
 let playerTurn = "O";
 let board = [
 	["", "", ""],
@@ -28,26 +18,18 @@ const addMarker = (id) => {
 		playerTurn = "X";
 	} else {
 		playerTurn = "O";
-  }
-  	board[row][column] = playerTurn; // updates the 'let board' JS
-	//checkForWin(); // marker  shows but alert doesn't show until until clicking an empty square
-  document.getElementById(id).textContent = playerTurn;
-  
-
-	//checkForWin(); //marker  shows but alert doesn't show until until clicking an empty square
-
-	//checkForWin(); //alert is thown but marker doesnt show up until cklicking the OK on the alert
-  console.log("board", board);
-  // checkForWin();
+	}
+	board[row][column] = playerTurn; // updates the 'let board' JS
+	document.getElementById(id).textContent = playerTurn;
+	console.log("board", board);
+	// checkForWin();
 };
 
 // is called when a square is clicked. "this" = element here.  This function updates the HTML
 const handleClick = (element) => {
 	if (!document.getElementById(element.id).textContent) {
-   
-    addMarker(element.id);
-  
-		checkForWin(); //keep this one
+		addMarker(element.id);
+		checkForWin(); //keep this one.  Other placements would have HTML be updated but the alert not show until the next click and/or alert the incorrect winner
 	}
 };
 
@@ -55,13 +37,13 @@ const checkForWin = () => {
 	// calls each checkForWin possibility and if any are true gives a page alert,
 	if (horizontalWin() || verticalWin() || diagonalWin()) {
 		// **BONUS** you could make the dismissal of this alert window reset the board...
-
-    // window.confirm(`Player ${playerTurn} won!`);
-    setTimeout( function ( ) { alert(`Player ${playerTurn} won!`); }, 10 );
-    
-		// document.getElementById("win").innemarHTML = `${playerTurn} WINS!`; //this works but the checkForWin needs to be initalized after
+		setTimeout(function() {
+			alert(`Player ${playerTurn} won!`);
+		}, 10); // setTimeout added to allow HTML to update before the alert appears.  If the alert appears first it stops operations until the 'ok' button is clicked and prevents the HTML from updating.
 		{
-		setTimeout( function(){(window.location.reload());}, 40);
+			setTimeout(function() {
+				window.location.reload();
+			}, 40);
 		}
 	}
 };
@@ -136,150 +118,17 @@ let diagonalWin = () => {
 };
 
 const resetBoard = () => {
-	playerTurn = 0;
+	playerTurn = "O";
 	// sanity check: this tells us the function is being called
 	console.log("the board was cleared!");
 
 	// collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp
 	const squares = document.getElementsByTagName("TD");
 
-	// loops over the HTML Collections and clears out the Xs and Os.  
-  for (i = 0; i<squares.length; i++){
-    squares[i].innerHTML = null;
-  }
-
-
-
-// // parseInt is updating the board in JS
-// const addMarker = (id) => {
-// 	// debugger;
-// 	// console.log(`We'll place a mark on square: ${id}`);
-// 	// console.log("id", id);
-// 	// checkForWin();
-// 	const row = parseInt(id.charAt(0));
-// 	const column = parseInt(id.charAt(2));
-// 	// checkForWin(); //gives alert but does add marker doesn't show until clicking the OK on the alert
-// 	if (playerTurn === "O") {
-// 		playerTurn = "X";
-// 	} else {
-// 		playerTurn = "O";
-// 	}
-// 	//checkForWin(); // marker  shows but alert doesn't show until until clicking an empty square
-//   document.getElementById(id).innerHTML = playerTurn;
-  
-  
-// 	//checkForWin(); //marker  shows but alert doesn't show until until clicking an empty square
-// 	board[row][column] = playerTurn; // updates the 'let board' JS
-// 	//checkForWin(); //alert is thown but marker doesnt show up until cklicking the OK on the alert
-// 	console.log("board", board);
-// };
-
-// // is called when a square is clicked. "this" = element here.  This function updates the HTML
-// const handleClick = (element) => {
-// 	if (!document.getElementById(element.id).innerHTML) {
-// 		addMarker(element.id);
-// 		checkForWin();
-// 	}
-// };
-
-// const checkForWin = () => {
-// 	// calls each checkForWin possibility and if any are true gives a page alert,
-// 	if (horizontalWin() || verticalWin() || diagonalWin()) {
-// 		// **BONUS** you could make the dismissal of this alert window reset the board...
-
-// 		alert(`Player ${playerTurn} won!`);
-// 		// document.getElementById("win").innemarHTML = `${playerTurn} WINS!`; //this works but the checkForWin needs to be initalized after
-// 		{
-// 			window.location.reload();
-// 		}
-// 	}
-// };
-
-// const horizontalWin = () => {
-// 	// console.log("horizontalWin");
-// 	if (board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") {
-// 		return true;
-// 	}
-// 	// console.log(board[0 && 0]);
-// 	if (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") {
-// 		return true;
-// 	}
-// 	if (board[0][2] == "X" && board[1][1] == "X" && board[2][2] == "X") {
-// 		return true;
-// 	}
-// 	if (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") {
-// 		return true;
-// 	}
-// 	if (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
-// 		return true;
-// 	}
-// 	if (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O") {
-// 		return true;
-// 	}
-// };
-
-// const verticalWin = () => {
-// 	// console.log("verticalWin");
-
-// 	if (board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") {
-// 		console.log("here1");
-// 		return true;
-// 	}
-// 	if (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") {
-// 		return true;
-// 	}
-// 	if (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") {
-// 		return true;
-// 	}
-// 	if (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") {
-// 		console.log("here2");
-// 		return true;
-// 	}
-// 	if (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
-// 		return true;
-// 	}
-// 	if (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O") {
-// 		return true;
-// 	}
-// };
-
-// const diagonalWin = () => {
-// 	// console.log("diagonalWin");
-
-// 	if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") {
-// 		// console.log("here1");
-// 		return true;
-// 	}
-// 	if (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") {
-// 		console.log("here2");
-// 		return true;
-// 	}
-// 	if (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
-// 		console.log("here3");
-// 		return true;
-// 	}
-// 	if (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O") {
-// 		console.log("here4");
-// 		return true;
-// 	}
-// };
-
-// const resetBoard = () => {
-// 	playerTurn = 0;
-// 	// sanity check: this tells us the function is being called
-// 	console.log("the board was cleared!");
-
-// 	// collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp
-// 	const squares = document.getElementsByTagName("TD");
-
-// 	// loops over the HTML Collections and clears out the Xs and Os.
-// 	for (i = 0; i < squares.length; i++) {
-// 		console.log(squares[i]);
-// 		squares[i].innerHTML = null;
-// 	}
-
-
-	// @TODO, Your code here: make sure to reset the array of arrays to empty for a new game
+	// loops over the HTML Collections and clears out the Xs and Os.
+	for (i = 0; i < squares.length; i++) {
+		squares[i].innerHTML = null;
+	}
 };
 
 // **BONUSES**
